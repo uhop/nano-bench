9a094368f13677556cdf55b93990c0d78833c4e8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-07-24 02:20:34 -0500

    Simplification.

--- src/kstest.js
+++ src/kstest.js
@@ -3,7 +3,8 @@
 
 // sup(F1(x) - F2(x))
 const sup = (sorted1, sorted2) => {
-  let size1 = sorted1.length, size2 = sorted2.length,
+  let size1 = sorted1.length,
+    size2 = sorted2.length,
     i = 0,
     j = 0,
     result = 0;
@@ -37,7 +38,7 @@ const kstest = (sorted1, sorted2, alpha = 0.05) => {
   const d = sup(sorted1, sorted2),
     n = sorted1.length,
     m = sorted2.length,
-    t = n < m ? (1 + n / m) / n : (1 + m / n) / m,
+    t = 1 / n + 1 / m,
     limit = Math.sqrt((-Math.log(alpha / 2) / 2) * t);
   return {value: d, limit, rejected: d > limit};
 };
