2ccfdb1670262424c0dbe19270d8ece74c3ea8ca
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-07-25 19:21:44 -0500

    Updated the test.

--- manual-tests/test-kwtest.js
+++ manual-tests/test-kwtest.js
@@ -1,15 +1,35 @@
 import {getParameters, rankData} from '../src/kwtest.js';
-import findCriticalValue from '../src/beta-critical.js';
+import betaPpf from '../src/beta-ppf.js';
+import chiSquaredPpf from '../src/chi-squared-ppf.js';
 
 const ALPHA = 0.05;
 
 const calculateH = (...groups) => {
-  const parameters = getParameters(groups),
-    H = rankData(groups),
-    Hc = findCriticalValue(1 - ALPHA, parameters.a, parameters.b) * parameters.nu;
+  const {a, b, nu, k, N} = getParameters(groups),
+    {H, T, S2, avgGroupRank} = rankData(groups),
+    Hc = betaPpf(1 - ALPHA, a, b) * nu;
 
-  console.log(parameters);
-  console.log(H, Hc, H > Hc);
+  console.log('parameters:', a, b, nu, k, N);
+  console.log(H, Hc, H > Hc ? 'STATISTICALLY SIGNIFICANT' : 'statistically insignificant');
+
+  if (H <= Hc) return;
+
+  // post-hoc tests
+
+  const C = chiSquaredPpf(1 - ALPHA / 2, N - k) * Math.sqrt((S2 * (N - 1 - T)) / (N - k));
+
+  console.log('\nStatistically significant difference between groups:');
+
+  for (let i = 0; i < k; ++i) {
+    for (let j = i + 1; j < k; ++j) {
+      const flag =
+        Math.abs(avgGroupRank[i] - avgGroupRank[j]) >
+        C * Math.sqrt(1 / groups[i].length + 1 / groups[j].length);
+      if (flag) {
+        console.log(i, j);
+      }
+    }
+  }
 };
 
 // Test case from: https://www.statisticshowto.com/probability-and-statistics/statistics-definitions/kruskal-wallis/
