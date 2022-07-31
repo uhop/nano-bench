20c3a1d9a890eecf20c56122b5ded0eee191795f
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-07-31 01:55:40 -0500

    Cleaned up the test removing a benchmarking.

--- manual-tests/test-kstest.js
+++ manual-tests/test-kstest.js
@@ -1,105 +1,27 @@
-import {measure} from '../src/runner.js';
-import {mean, bootstrap, getWeightedValue} from '../src/stats.js';
-import {prepareTimeFormat, formatTime, abbrNumber} from '../src/formatters.js';
 import kstest from '../src/kstest.js';
 
 const ALPHA = 0.05; // confidence is 95%
 
 const numericSortingAsc = (a, b) => a - b;
 
-const print = (label, stats, nSeries) => {
-  const reps = stats.reps;
-  stats.ensureSorted().normalizeReps();
+const testGroups = (group1, group2) => {
+  group1.sort(numericSortingAsc);
+  group2.sort(numericSortingAsc);
 
-  // classics
-  // const median = getWeightedValue(stats.data),
-  //   lo = getWeightedValue(stats.data, ALPHA / 2),
-  //   hi = getWeightedValue(stats.data, 1 - ALPHA / 2),
-  //   timeFormat = prepareTimeFormat([median, lo, hi, median - lo, hi - median], 1000),
-  //   plus = formatTime(hi - median, timeFormat),
-  //   minus = formatTime(median - lo, timeFormat);
+  const results = kstest(group1, group2, ALPHA);
 
-  // bootstrap
-  const median = mean(
-      bootstrap(samples => getWeightedValue(samples.sort(numericSortingAsc)), stats.data)
-    ),
-    lo = mean(
-      bootstrap(samples => getWeightedValue(samples.sort(numericSortingAsc), ALPHA / 2), stats.data)
-    ),
-    hi = mean(
-      bootstrap(
-        samples => getWeightedValue(samples.sort(numericSortingAsc), 1 - ALPHA / 2),
-        stats.data
-      )
-    ),
-    timeFormat = prepareTimeFormat([median, lo, hi, median - lo, hi - median], 1000),
-    plus = formatTime(hi - median, timeFormat),
-    minus = formatTime(median - lo, timeFormat);
-
-  console.log(
-    label,
-    'median',
-    formatTime(median, timeFormat),
-    plus === minus ? '±' + plus : '+' + plus + ' -' + minus,
-    'for',
-    abbrNumber(reps),
-    'iterations in',
-    abbrNumber(nSeries),
-    'series'
-  );
+  console.log('results:', results);
+  console.log(results.rejected ? 'STATISTICALLY SIGNIFICANT' : 'statistically insignificant');
 };
 
-const compare = async (label1, fn1, label2, fn2, options = {}, options1 = {}, options2 = {}) => {
-  const stats1 = await measure(fn1, Object.assign({}, options, options1));
-  stats1.ensureSorted();
-
-  const stats2 = await measure(fn2, Object.assign({}, options, options2));
-  stats2.ensureSorted();
-
-  const {alpha = ALPHA} = options || {};
-  const results = kstest(stats1.data, stats2.data, alpha);
-
-  print(label1, stats1, options1?.nSeries || options?.nSeries || 100);
-  print(label2, stats2, options2?.nSeries || options?.nSeries || 100);
-  console.log(
-    'The difference is statistically ' +
-      (results.rejected ? 'SIGNIFICANT' : 'insignificant') +
-      '.\n'
-  );
-};
-
-await compare(
-  'String:',
-  n => {
-    let x = '';
-    const a = 'a',
-      b = 'b';
-    for (let i = 0; i < n; ++i) x = a + '-' + b;
-  },
-  'Backticks:',
-  n => {
-    let x = '';
-    const a = 'a',
-      b = 'b';
-    for (let i = 0; i < n; ++i) x = `${a}-${b}`;
-  }
+// Test case form: https://www.statext.com/practice/KolmogorovSmirnovT04.php
+testGroups(
+  [497, 839, 798, 892, 1585, 755, 388, 617, 248, 1641, 1180, 619, 253, 661, 1981, 1746, 1865, 238, 1199, 1524], // men
+  [820, 184, 921, 488, 721, 614, 801, 396, 864, 845, 404, 781, 457, 1029, 1047, 552, 718, 495, 382, 1090] // women
 );
 
-await compare(
-  'String fn:',
-  n => {
-    let x = '';
-    const a = 'a',
-      b = 'b';
-    const fn = (...args) => args.join('');
-    for (let i = 0; i < n; ++i) x = fn(a, '-', b);
-  },
-  'Backtick fn:',
-  n => {
-    let x = '';
-    const a = 'a',
-      b = 'b';
-    const fn = (parts, ...args) => parts[0] + args.map((arg, i) => arg + parts[i + i]).join('');
-    for (let i = 0; i < n; ++i) x = fn`${a}-${b}`;
-  }
-);
+// Test case from: https://www.ncl.ucar.edu/Document/Functions/Built-in/kolsm2_n.shtml
+testGroups(
+  [15.7, 16.1, 15.9, 16.2, 15.9, 16.0, 15.8, 16.1, 16.3, 16.5, 15.5],
+  [15.4, 16.0, 15.6, 15.7, 16.6, 16.3, 16.4, 16.8, 15.2, 16.9, 15.1]
+)
