b097b599aad63ce9881b399bf92340f425a276d7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-07-31 01:56:31 -0500

    Made a combined test for all three tests: MW, KS, KW.

---
+++ manual-tests/test-combined.js
@@ -0,0 +1,31 @@
+import mwtest from '../src/mwtest.js';
+import kstest from '../src/kstest.js';
+import kwtest from '../src/kwtest.js';
+
+const ALPHA = 0.05; // confidence is 95%
+
+const numericSortingAsc = (a, b) => a - b;
+
+const testGroups = (group1, group2) => {
+  group1.sort(numericSortingAsc);
+  group2.sort(numericSortingAsc);
+
+  const results = mwtest(group1, group2, ALPHA);
+
+  console.log('===');
+  console.log('mwtest:', mwtest(group1, group2, ALPHA).rejected);
+  console.log('kstest:', kstest(group1, group2, ALPHA).rejected);
+  console.log('kwtest:', kwtest([group1, group2], ALPHA).rejected);
+};
+
+// Test case form: https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test
+testGroups(
+  [6, 1, 1, 1, 1, 1], // tortoises
+  [5, 5, 5, 5, 5, 0] // hares
+);
+
+// Test case form: https://sphweb.bumc.bu.edu/otlt/mph-modules/bs/bs704_nonparametric/bs704_nonparametric4.html
+testGroups(
+  [8, 7, 6, 2, 5, 8, 7, 3], // usual care
+  [9, 9, 7, 8, 10, 9, 6] // new program
+);
