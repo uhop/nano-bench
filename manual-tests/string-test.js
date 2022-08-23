eb4d52693d7072b90ed961ddc8e74b9a2b17e6b7
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-08-23 01:06:48 -0500

    Simple test to measure string operations.

---
+++ manual-tests/string-test.js
@@ -0,0 +1,33 @@
+import show from '../src/show.js';
+
+await show({
+  strings: n => {
+    let x = '';
+    const a = 'a',
+      b = 'b';
+    for (let i = 0; i < n; ++i) x = a + '-' + b;
+  },
+  backticks: n => {
+    let x = '';
+    const a = 'a',
+      b = 'b';
+    for (let i = 0; i < n; ++i) x = `${a}-${b}`;
+  }
+});
+
+await show({
+  "strings-fn": n => {
+    let x = '';
+    const a = 'a',
+      b = 'b';
+    const fn = (...args) => args.join('');
+    for (let i = 0; i < n; ++i) x = fn(a, '-', b);
+  },
+  "backticks-fn": n => {
+    let x = '';
+    const a = 'a',
+      b = 'b';
+    const fn = (parts, ...args) => parts[0] + args.map((arg, i) => arg + parts[i + i]).join('');
+    for (let i = 0; i < n; ++i) x = fn`${a}-${b}`;
+  }
+});
